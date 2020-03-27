7dc76acb7b0ddf07600176ee5392257e90b9c598
ykud <mail@ykud.com>
2020-03-27 17:44:23 +1100

    adding mdx view definitions, cleaning up logging

--- tm1diff.py
+++ tm1diff.py
@@ -16,13 +16,27 @@ from TM1py.Services import TM1Service
 from TM1py.Utils import CaseAndSpaceInsensitiveTuplesDict
 from ast import literal_eval
 
-def query_cube_view (config, tm1_server_name, cube_name, view_name):
+def query_cube_view (config, view_definition):
 	view_read_start_time =  time.time()
-	
-	logging.debug('%s.%s.%s view starting to read data'%(tm1_server_name, cube_name, view_name))
-	with TM1Service(**config[tm1_server_name], timeout=50) as tm1:
+	tm1_server_name = view_definition ['server'] 
+	cube_name = view_definition ['view']['cube']
+	if view_definition['view']['type']=='native':
+		view_name = view_definition ['view']['name']
+		with TM1Service(**config[tm1_server_name], timeout=50) as tm1:
 			cell_set = tm1.cubes.cells.execute_view (cube_name, view_name, private=False)
-	logging.info('%s.%s.%s view with %d cells read in %.2f'%( tm1_server_name, cube_name, view_name, len(cell_set),time.time()-view_read_start_time))
+	elif view_definition['view']['type']=='mdx':
+		mdx_query = view_definition['view']['mdx']
+		view_name = "MDX query at %s" % (time.strftime("%Y%m%d-%H%M%S"))
+		with TM1Service(**config[tm1_server_name], timeout=50) as tm1:
+			cell_set = tm1.cubes.cells.execute_mdx(mdx_query)
+	#elif view_definition['view']['type']=='random':
+		# this is where we try to create a new view and somehow 'randomise it' by selecting different elements
+		# we are going to record this view back as MDX view based on whatever we generated randomly
+		# view_definition['view']['mdx'] = whatever we generate converted to mdx
+	else: 
+		logging.error("View type %s in test definition is not implemented" % (view_definition['view']['type']) )
+		raise Exception('View type not implemented')
+	logging.info('%s.%s.%s view with %d cells read in %.2f'%( tm1_server_name, cube_name, view_name, len(cell_set),time.time()-view_read_start_time)) 
 	return cell_set
 
 def write_cell_set_to_file(cell_set, file_name):
@@ -62,13 +76,13 @@ def compare_cell_sets(check_file, cell_set_source, cell_set_target):
 		field_names = ['change_type', 'cell', 'source_value','target_value']
 		csv_writer = csv.DictWriter(diff_file, fieldnames=field_names, quotechar='"',quoting=csv.QUOTE_ALL)
 		csv_writer.writeheader()
-		logging.warning ("Source and target views do not match, please find list of differences (first 10 lines) below, the rest will be recorded in the file %s" %(file_name_to_write_diff))
-		num_lines = 0
+		num_of_variances = 0
 		for diff in dictdiffer.diff(cell_set_source, cell_set_target):
-			#csv_writer.write(str(diff))
-			if num_lines <= 10: logging.warning (diff)
-			num_lines += 1
+			#TODO: need to check the magnitude of difference, cause we might get 0.01 variances on large consolidations in different instances
+			
 			csv_writer.writerow({'change_type':diff[0], 'cell':diff[1],'source_value':diff[2][0], 'target_value':diff[2][1]})
+			num_of_variances += 1
+		logging.warning ("Source and target views do not match, %n cells vary, please find list of differences in the file %s" %(num_of_variances ,file_name_to_write_diff))
 		return False
 	else:
 		logging.info ("Source and target views match")
@@ -89,34 +103,34 @@ def generate_reverse_json_check_file(check_file_name, original_json,  target_exp
 def run_check(input_file, config):
 	# open config file 
 	# TODO:-- need validations here for JSON existence, formats and all other things -- it will fail over with exception if read something wrong anyway
-	with open(input_file) as test_json:
-  		test_config = json.load(test_json)
-	logging.info('Start running test %s'%(test_config['test_name']))
+	with open(input_file) as check_json:
+  		check_definition = json.load(check_json)
+	logging.info('Start running test %s'%(check_definition['test_name']))
 	# TODO: need to check source and target test types
-	if test_config['source']['type'] == 'tm1':
-		cell_set_source = query_cube_view(config, test_config['source']['server'], test_config['source']['view']['cube'], test_config['source']['view']['name'])
-	elif test_config['source']['type'] == 'file':
-		cell_set_source = read_cell_set_from_file(test_config['source']['filename'])
+	if check_definition['source']['type'] == 'tm1':
+		cell_set_source = query_cube_view(config, check_definition['source'])
+	elif check_definition['source']['type'] == 'file':
+		cell_set_source = read_cell_set_from_file(check_definition['source']['filename'])
 	else:
-		logging.error("Source type %s in test definition is not implemented" % (test_config['source']['type']) )
+		logging.error("Source type %s in test definition is not implemented" % (check_definition['source']['type']) )
 		raise Exception('Source type not implemented')
 
-	if test_config['target']['type'] == 'tm1':
-		cell_set_target = query_cube_view(config, test_config['target']['server'], test_config['target']['view']['cube'], test_config['target']['view']['name'])
-	elif test_config['target']['type'] == 'file':
+	if check_definition['target']['type'] == 'tm1':
+		cell_set_target = query_cube_view(config, check_definition['target'])
+	elif check_definition['target']['type'] == 'file':
 		# write results to the file
 		# need a special export folder? 
 		export_folder = 'test_exports'
-		if test_config['target']['folder'] != '': export_folder = test_config['target']['folder']
+		if check_definition['target']['folder'] != '': export_folder = check_definition['target']['folder']
 		Path(export_folder).mkdir(parents=True, exist_ok=True)
 		file_name_to_export = join(export_folder,"%s_%s_export_data.csv" % (Path(input_file).stem,time.strftime("%Y%m%d-%H%M%S")))
 		write_cell_set_to_file(cell_set_source,file_name_to_export)
 		# generate a 'reverse' check definition to be able to run backwards comparison easily
-		generate_reverse_json_check_file(input_file, test_config, file_name_to_export)
+		generate_reverse_json_check_file(input_file, check_definition, file_name_to_export)
 		# stop comparing
 		return True
 	else:
-		logging.error("Target type %s in test definition is not implemented" % (test_config['target']['type']) )
+		logging.error("Target type %s in test definition is not implemented" % (check_definition['target']['type']) )
 		raise Exception('Target type not implemented')
 	
 	return compare_cell_sets(input_file, cell_set_source, cell_set_target)
@@ -151,6 +165,8 @@ def main (argv):
                             format='%(asctime)s,%(msecs)d %(threadName)s %(levelname)s %(message)s',
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
+	# adding the console logger as well
+	logging.getLogger().addHandler(logging.StreamHandler())
 	logging.info("Starting to run tests from %s with %s threads, output log to %s" %(input_file_or_folder, number_of_threads, log_file))						
 	starttime_full=time.time()
 	num_tests = 0
